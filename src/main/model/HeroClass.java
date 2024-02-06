138fb8e8613bb100f34abd5894189b1d31294d9e
Kyle Li <likyle701@gmail.com>
2024-02-06 01:13:25 -0800

    Added Some Classes

---
+++ src/main/model/HeroClass.java
@@ -0,0 +1,49 @@
+package model;
+
+public abstract class HeroClass {
+    protected int health;
+    protected int attack;
+
+    public HeroClass(int health, int attack) {
+        this.health = health;
+        this.attack = attack;
+    }
+
+    // getters
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    // setters
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    //EFFECTS: false if hero is alive, else drops gold and item if hero is dead
+    public boolean dead() {
+        boolean state = false;
+        if (health <= 0) {
+            state = true;
+            droppedItem();
+            droppedGold();
+        }
+        return state;
+    }
+
+    //EFFECTS: returns amount of gold dropped by enemy
+    protected abstract int droppedGold();
+
+    //EFFECTS: returns item dropped by enemy
+    protected abstract Item droppedItem();
+
+
+}
+
